512b07108df769c91347d0f961f6ba036a70f429
sitiaaisyah <148614204+sitiaaisyah@users.noreply.github.com>
2023-11-21 21:03:48 +0700

    Add files via upload

---
+++ Pertemuan6/Frmtranslator.py
@@ -0,0 +1,67 @@
+from tkinter import Frame,Label,Entry,Button,YES,BOTH,END,Tk,W
+from googletrans import Translator
+
+class FrmTranslator:
+    def __init__(self, parent, title):
+        self.parent = parent       
+        self.parent.geometry("500x200")
+        self.parent.title(title)
+        self.parent.protocol("WM_DELETE_WINDOW", self.onKeluar)
+        self.aturKomponen()
+        
+    def aturKomponen(self):
+        mainFrame = Frame(self.parent, bd=10, bg="light blue")
+        mainFrame.pack(fill=BOTH, expand=YES)
+
+        # pasang Label
+        Label(mainFrame, text='Masukkan teks:', bg="pink").grid(row=0, column=0,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text='Hasil Korea:', bg="pink").grid(row=2, column=0,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text='Hasil sunda:', bg="pink").grid(row=4, column=0,
+            sticky=W, padx=5, pady=5)
+        Label(mainFrame, text='Hasil Arabic:', bg="pink").grid(row=6, column=0,
+            sticky=W, padx=5, pady=5)
+        
+        # pasang textbox
+        self.txtSumber = Entry(mainFrame, width=50) 
+        self.txtSumber.grid(row=0, column=1, padx=5, pady=5)
+
+        self.txtHasil = Entry(mainFrame, width=50) 
+        self.txtHasil.grid(row=2, column=1, padx=5, pady=5)
+        
+        self.txtHasil2 = Entry(mainFrame, width=50) 
+        self.txtHasil2.grid(row=4, column=1, padx=5, pady=5)
+        
+        self.txtHasil3 = Entry(mainFrame, width=50) 
+        self.txtHasil3.grid(row=6, column=1, padx=5, pady=5)
+        
+        Label(mainFrame, text='Siti Aisyah 220511090', bg="pink").grid(row=8, column=1, pady=5, padx=5)
+
+        # Pasang Button
+        self.btnTranslate = Button(mainFrame, text='Translate!',
+            command=self.onTranslate, bg="pink")
+        self.btnTranslate.grid(row=1, column=1, padx=5, pady=5) 
+
+    def onTranslate(self):
+        # membuat instance object
+        penterjemah = Translator()
+
+        # menterjemahkan
+        hasil = penterjemah.translate(self.txtSumber.get(), src='id', dest='ko') 
+        hasil2 = penterjemah.translate(self.txtSumber.get(), src='id', dest='su') 
+        hasil3 = penterjemah.translate(self.txtSumber.get(), src='id', dest='ar') 
+       
+        # menampilkan hasil terjemahan
+        self.txtHasil.insert(END,hasil.text)
+        self.txtHasil2.insert(END,hasil2.text)
+        self.txtHasil3.insert(END,hasil3.text)
+        
+    def onKeluar(self, event=None):
+        # memberikan perintah menutup aplikasi
+        self.parent.destroy()
+
+if __name__ == '__main__':
+    root = Tk()  
+    aplikasi = FrmTranslator(root, "Program Translator")
+    root.mainloop() 
